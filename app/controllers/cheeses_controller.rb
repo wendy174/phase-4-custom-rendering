400d0923f88cdbb27af0e782bafd8400b1cae642
wendy <mizukilee@gmail.com>
2023-02-19 19:39:41 -0500

    commit tree 5b516140547a5d98c8e99d077bdbe3e7af6627c5

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,23 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # only inlcudes attributes you named 
+  # except will exclude attributes you indicate 
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name], methods: [:summary]
   end
 
   # GET /cheeses/:id
+  # custome error message 
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+    if cheese 
+      render json: cheese
+    else 
+      render json: {error: "Cheese not found"}, status: 404 
+    end 
+  end 
+  
 
 end
